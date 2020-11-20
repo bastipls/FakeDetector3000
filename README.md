e8f1b709f4efb06cd2662049f6f2bdee4dac78ee
Bastian Perez <bas.perezp@alumnos.duoc.cl>
2020-11-20 15:57:19 -0300

    Agrego readme y quito comentarios

---
+++ README.md
@@ -0,0 +1,13 @@
+# Api FakeDetector3000 :tw-1f4f0:
+
+Nuestra aplicación orientada, pero no limitada a smartphone, está dirigida a todo tipo de usuario que desee verificar la veracidad de una noticia, es sencilla e intuitiva de usar por lo cual la edad no será una dificultad a la hora de utilizar nuestra aplicación.
+
+Es capaz de analizar el link de una noticia ya sea en español o inglés, con un porcentaje de asertividad del 80 y 96 % respectivamente, fue pensada para el uso fácil y en pocos pasos, solo es necesario copiar el link de una noticia que necesitas verificar y fakedetector3000, mediante un moderno sistema de inteligencia artificial, analizará ciertos elementos del sitio, desde el número de palabras, hasta la conjugación de estas, todo en tiempo real, dando al usuario un resultado en casi de manera instantánea.
+
+Como equipo somos conscientes del efecto de las noticias falsas, por lo que nos sentimos comprometidos en realizar un cambio por el bien de las personas, nuestra aplicación es capaz de identificar noticias falsas en cuestión de segundos, logrando así interrumpir el flujo de fake news y el caos que generan dentro de la sociedad. Por otro lado, buscamos que mediante nuestra aplicación los usuarios obtengan un “Feedback” positivo, educandose y comprendiendo de mejor manera el mundo digital y la gravedad que conlleva el compartir fake news.
+
+
+### Mas informacion :tw-2b07:
+
+Para probar nuestro prototipo [Link](https://fakedetector3000.tools/ "Link")
+
--- fakedetector/views.py
+++ fakedetector/views.py
@@ -57,12 +57,9 @@ def search(noticiaUrl):
 
     web = noticiaUrl
     req = Request(web, headers={'User-Agent': 'Mozilla/5.0'})
-    # datos = urllib.request.urlopen(web).read().decode()
     datos = urlopen(req).read()
     soup =  BeautifulSoup(datos,'html.parser')
-    #tags = soup('p')
     tags = soup.find_all("p")
-    #title = soup.find('title')
     title = soup.find("meta",  property="og:title")
     if title:
         texto = title["content"] +' '+ ''.join(str(tag.text) for tag in tags)
@@ -107,46 +104,26 @@ def predecir(noticia):
 
 def validar_url(url):
     regex = re.compile(
-        r'^(?:http|ftp)s?://' # http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'^(?:http|ftp)s?://' # http:// O https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #dominio...
         r'localhost|' #localhost...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
-        r'(?::\d+)?' # optional port
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ..si es ip
+        r'(?::\d+)?' # Esto es opcional
         r'(?:/?|[/?]\S+)$', re.IGNORECASE)
     resutlado = re.match(regex, url) is not None
     return resutlado
-
 # Get aun no terminado
 class prediccion(APIView):
-    # def get(self,request):
-    #     nombre_archivo_modelo = 'randomforestIngles.sav'
-    #     nombre_archivo_transform = 'stringtomatrizIngles.sav'
-    #     loaded_model = pickle.load(open('./modelos/'+nombre_archivo_modelo, 'rb'))
-    #     load_model_matriz = pickle.load(open('./modelos/'+nombre_archivo_transform, 'rb'))
-    #     noticia = 'Bitter John McCain Calls Trump ‘Ill Informed’ in Nasty OpEd: ‘We don’t answer to him’What the heck! Senator John McCain just admitted that he doesn t answer to President Trump! Can you imagine if a Senator had done this to Obama? McCain s OpEd in The Washington Post is a horrible hit on Trump. Shame on him!It starts out with a statement about how horrible the white supremacists are and then goes on to discuss Congress getting back to order. It s a snoozer of an OpEd until you get to the last part In a shocking slam on President Trump s character and knowledge of government, McCain shows his bitterness towards Trump. He claims that,  We don t answer to him   but also say that,  We must respect his authority WTH!MCCAIN: IT S TIME CONGRESS RETURNS TO REGULAR ORDER: We can fight like hell for our ideas to prevail. But we have to respect each other or at least respect the fact that we need each other.That has never been truer than today, when Congress must govern with a president who has no experience of public office, is often poorly informed and can be impulsive in his speech and conduct.We must respect his authority and constitutional responsibilities. We must, where we can, cooperate with him. But we are not his subordinates. We don t answer to him. We answer to the American people. We must be diligent in discharging our responsibility to serve as a check on his power. And we should value our identity as members of Congress more than our partisan affiliation.I argued during the health-care debate for a return to regular order, letting committees of jurisdiction do the principal work of crafting legislation and letting the full Senate debate and amend their efforts.We won t settle all our differences that way, but such an approach is more likely to make progress on the central problems confronting our constituents. We might not like the compromises regular order requires, but we can and must live with them if we are to find real and lasting solutions. And all of us in Congress have the duty, in this sharply polarized atmosphere, to defend the necessity of compromise before the American public.SEE WHAT HE DID THERE? He basically said our president is clueless and impulsive so we don t need to listen to him. Unreal! Is McCain bitter and trying to get back at Trump? It s unheard of for a sitting Senator of the party in power trashes the president that is from the same party! Read more: WaPo'
-    #     sin_limpiar = noticia
-    #     noticia = limpiar_stopwrods(noticia)
-    #     testdta = pd.Series([noticia])
-    #     a_predecir = load_model_matriz.transform(testdta)
-    #     resultado = loaded_model.predict(a_predecir)
-    #     # Detectar el idioma de la noticia
-    #     idioma = detect(noticia)
-    #     prediccion = str(resultado[0])
-    #     print('BODY============',request.data)
-    #     # print('Lenguaje ',detect(noticia))
-    #     # print('Resultado =========== ',resultado[0])
-    #     # return HttpResponse('asdasd')
-    #     json = {'prediccion':prediccion,'idioma':idioma,'cuerpo':noticia}
-    #     my_serializer = NoticiaSerializer(data=json)
-    #     my_serializer.is_valid(True)
-    
-    #     print(type(request.data))
-    #     return Response(my_serializer.data)
-
-
-
+    def options(self,request):
+        json = {'post_method':{'cuerpo':'Url o cuerpo de la noticia mas su titulo (Noticias en ingles o español)'},
+                'response':{'cuerpo':'Cuerpo de la noticia extraida de la url',
+                            'prediccion:':'1 = Verdadero, 0 = Falso',
+                            'idioma':'Idioma detectado de la noticia'}}
+        return Response(json)
     def post(self, request):
+
         validar_campos = NoticiaSerializer(data=request.data)
+
         if validar_campos.is_valid() == False :
             return Response(validar_campos.errors,status=status.HTTP_400_BAD_REQUEST)
         elif  type(request.data['cuerpo'])  != str: 
@@ -156,7 +133,6 @@ class prediccion(APIView):
             noticia = search(noticia)
         else:
             noticia = request.data['cuerpo']
-        
         
         json = predecir(noticia)
         my_serializer = NoticiaSerializer(data=json)
